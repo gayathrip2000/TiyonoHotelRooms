6ea9a88f1a33419e7e9e5a834ef6b895002c2967
shanuka27 <shanukayasiru27@gmail.com>
2025-05-01 18:38:21 +0530

    Add validation to check past days

--- cypress/e2e/Searchbar.cy.js
+++ cypress/e2e/Searchbar.cy.js
@@ -13,9 +13,24 @@ describe('Search Room', () => {
 
       cy.get('.ant-picker-input-active > input').click().type("07-05-2025") // enter start date
 
-      cy.get(':nth-child(3) > input').click().type("06-05-2025") // enter end date
+      cy.get(':nth-child(3) > input').click().type("06-04-2025") // enter end date
 
-      cy.get('.col-md-5 > .form-control').type("quad hotel room Type")
+      cy.get(':nth-child(3) > input').invoke('val').then((endDate) => {
+        // Get the current date
+        const currentDate = new Date();
+        const formattedCurrentDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+  
+        const [day, month, year] = endDate.split('-');
+        const enteredEndDate = new Date(year, month - 1, day); // Month is 0-indexed
+  
+        if (enteredEndDate < formattedCurrentDate) {
+          throw new Error('End date cannot be in the past. Test case failed!');
+        } else {
+          cy.log('End date is valid');
+        }
 
+        cy.get('.col-md-5 > .form-control').type("quad hotel room Type")
+
+       })
     })
 })
